08f950ddb03502edc07092f2d7973f3dec39a8a4
Yun Jang <jang7j@yahoo.com>
2022-08-20 21:40:24 -0500

    initial commit

---
+++ CodeWars/Program.cs
@@ -0,0 +1,9 @@
+﻿static string MakeUpperCase(string str)
+{
+    return str.ToUpper();
+
+}
+
+Console.WriteLine("Write something: ");
+var input = Console.ReadLine();
+Console.WriteLine(MakeUpperCase(input));
